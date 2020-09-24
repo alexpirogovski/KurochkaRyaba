f4df4141db23ec82a395d19f0d5145358e82d064
alexpirogovski <pirogovski@gmail.com>
2020-09-24 21:15:27 +0300

    Added comments and optimization

--- skazka_1.py
+++ skazka_1.py
@@ -1,26 +1,23 @@
-import time
-import random
+import time  # Need this to sleep
+import random  # Need this to get random numbers
 
 
 class KurochkaRyaba(object):
   def __init__(self):
-    print('I byla u nih kurochka ryaba')
-    # Play with this number to increase the number of normal eggs
+    print('I byla u nih kurochka ryaba.')  # This will be printed second
+    # Increase with this number to get more normal eggs
     random.seed(2)
 
   def lay_egg(self):
-    egg_type = random.randint(0,10)
-    if egg_type <= 5:
-      egg = 'Normal'
-    else:
-      egg = 'Gold'
-    print('The Kurochka has layed a {} egg'.format(egg))
+    egg_type = bool(random.getrandbits(1))  # This is the fastest according to https://stackoverflow.com/questions/6824681/get-a-random-boolean-in-python
+    egg = 'Normal' if egg_type else 'Golden'  # Python syntax
+    print('The Kurochka has layed a {} egg.'.format(egg))
     return egg
 
 
 class DedAndBabka(object):
   def __init__(self):
-    print('Zhili-byli ded da baba')
+    print('Zhili-byli ded da baba.')  # This will be printed first
     self.kurochka = KurochkaRyaba()
 
   def live(self):
@@ -29,7 +26,7 @@ class DedAndBabka(object):
       if egg == 'Normal':
         # Reduce this number for a faster simulation
         time.sleep(1)
-      elif egg == 'Gold':
+      elif egg == 'Golden':
         break
       else:
         print('Unknown egg type!')
